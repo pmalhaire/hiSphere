f9eb765f43d84346186144a78b07cfa798921dd8
pmalhaire <pierre.malhaire@gmail.com>
2020-05-03 19:02:52 +0200

    create circle

--- game.cpp
+++ game.cpp
@@ -7,192 +7,176 @@
 
 #include "game.hpp"
 
-#define enum_case(str)\
-	case str:\
-	std::cerr << SDL_GetTicks() << "--" << file << ", " << line << ": " #str "\n";\
-	break;
+#define enum_case(str)                                                         \
+  case str:                                                                    \
+    std::cerr << SDL_GetTicks() << "--" << file << ", " << line                \
+              << ": " #str "\n";                                               \
+    break;
 
+void gl_error_check(std::string file, int line) {
+  GLenum err = glGetError();
 
-void gl_error_check(std::string file, int line)
-{
-	GLenum err = glGetError();
-
-	switch (err) {
-		enum_case(GL_INVALID_ENUM)
-		enum_case(GL_INVALID_VALUE)
-		enum_case(GL_INVALID_OPERATION)
-		enum_case(GL_INVALID_FRAMEBUFFER_OPERATION)
-		enum_case(GL_OUT_OF_MEMORY)
-		default:
-			break;
-	}
+  switch (err) {
+    enum_case(GL_INVALID_ENUM) enum_case(GL_INVALID_VALUE)
+        enum_case(GL_INVALID_OPERATION)
+            enum_case(GL_INVALID_FRAMEBUFFER_OPERATION)
+                enum_case(GL_OUT_OF_MEMORY) default : break;
+  }
 }
 
-#define GL_ERR_CHECK \
-do {\
-gl_error_check(__FILE__, __LINE__); \
-} while(0)
-
+#define GL_ERR_CHECK                                                           \
+  do {                                                                         \
+    gl_error_check(__FILE__, __LINE__);                                        \
+  } while (0)
 
 class game_impl {
 public:
-	virtual void draw() = 0;
+  virtual void draw() = 0;
 };
 
 class game_impl3 : public game_impl {
 
-GLuint program_object_, vbuffer_;
+  GLuint program_object_, vbuffer_;
 
-GLuint LoadShader(GLenum type, const char *shaderSrc)
-{
-   GLuint shader;
-   GLint compiled;
-   // Create the shader object
-   shader = glCreateShader(type);
-   if(shader == 0)
+  GLuint LoadShader(GLenum type, const char *shaderSrc) {
+    GLuint shader;
+    GLint compiled;
+    // Create the shader object
+    shader = glCreateShader(type);
+    if (shader == 0)
       return 0;
-   // Load the shader source
-   glShaderSource(shader, 1, &shaderSrc, NULL);
-   // Compile the shader
-   glCompileShader(shader);
-   // Check the compile status
-   glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    // Load the shader source
+    glShaderSource(shader, 1, &shaderSrc, NULL);
+    // Compile the shader
+    glCompileShader(shader);
+    // Check the compile status
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
 
-   if(!compiled) 
-   {
+    if (!compiled) {
       GLint infoLen = 0;
       glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-      if(infoLen > 1)
-      {
-         char* infoLog = static_cast<char*>(malloc(sizeof(char) * infoLen));
-         glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
-         fprintf(stderr, "Error compiling shader:\n%s\n", infoLog);
-         free(infoLog);
+      if (infoLen > 1) {
+        char *infoLog = static_cast<char *>(malloc(sizeof(char) * infoLen));
+        glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+        fprintf(stderr, "Error compiling shader:\n%s\n", infoLog);
+        free(infoLog);
       }
       glDeleteShader(shader);
       return 0;
-   }
-   return shader;
-}
-///
-// Initialize the shader and program object
-//
-bool Init()
-{
-   const char vShaderStr[] =  
-      "attribute vec4 vPosition;   \n"
-      "void main()                 \n"
-      "{                           \n"
-      "   gl_Position = vPosition; \n"
-      "}                           \n";
-   const char fShaderStr[] =  
+    }
+    return shader;
+  }
+  ///
+  // Initialize the shader and program object
+  //
+#define N_VERT 50
+  bool Init() {
+    const char vShaderStr[] = "attribute vec4 vPosition;   \n"
+                              "void main()                 \n"
+                              "{                           \n"
+                              "   gl_Position = vPosition; \n"
+                              "}                           \n";
+    const char fShaderStr[] =
 #ifdef EMSCRIPTEN
-      "precision mediump float;                   \n"
+        "precision mediump float;                   \n"
 #endif
-      "void main()                                \n"
-      "{                                          \n"
-      "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
-      "}                                          \n";
-   GLuint vertexShader;
-   GLuint fragmentShader;
-   GLuint programObject;
-   GLint linked;
-   // Load the vertex/fragment shaders
-   vertexShader = LoadShader(GL_VERTEX_SHADER, vShaderStr);
-   fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
-   // Create the program object
-   programObject = glCreateProgram();
-   if(programObject == 0)
+        "void main()                                \n"
+        "{                                          \n"
+        "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
+        "}                                          \n";
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    GLuint programObject;
+    GLint linked;
+    // Load the vertex/fragment shaders
+    vertexShader = LoadShader(GL_VERTEX_SHADER, vShaderStr);
+    fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fShaderStr);
+    // Create the program object
+    programObject = glCreateProgram();
+    if (programObject == 0)
       return 0;
-   glAttachShader(programObject, vertexShader);
-   glAttachShader(programObject, fragmentShader);
-   // Bind vPosition to attribute 0   
-   glBindAttribLocation(programObject, 0, "vPosition");
-   // Link the program
-   glLinkProgram(programObject);
-   // Check the link status
-   glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
-   if(!linked) 
-   {
+    glAttachShader(programObject, vertexShader);
+    glAttachShader(programObject, fragmentShader);
+    // Bind vPosition to attribute 0
+    glBindAttribLocation(programObject, 0, "vPosition");
+    // Link the program
+    glLinkProgram(programObject);
+    // Check the link status
+    glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
+    if (!linked) {
       GLint infoLen = 0;
       glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
-      if(infoLen > 1)
-      {
-         char* infoLog = static_cast<char *>(malloc(sizeof(char) * infoLen));
-         glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
-         fprintf(stderr, "Error linking program:\n%s\n", infoLog);
-         free(infoLog);
+      if (infoLen > 1) {
+        char *infoLog = static_cast<char *>(malloc(sizeof(char) * infoLen));
+        glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
+        fprintf(stderr, "Error linking program:\n%s\n", infoLog);
+        free(infoLog);
       }
       glDeleteProgram(programObject);
       return false;
-   }
-	   GL_ERR_CHECK;
+    }
+    GL_ERR_CHECK;
 
-   // Store the program object
-   program_object_ = programObject;
+    // Store the program object
+    program_object_ = programObject;
+    GLfloat vVertices[N_VERT * 3] = {};
 
-   // Load the vertex buffer
-   GLfloat vVertices[] = {0.0f,  0.5f, 0.0f, 
-                          -0.5f, -0.5f, 0.0f,
-                          0.5f, -0.5f,  0.0f};
+    int idx = 0;
+    for (float i = 0.0f; i < 2 * M_PI; i += 2.0 * M_PI / N_VERT) {
+      vVertices[idx] = cos(i);
+      vVertices[idx + 1] = sin(i);
+      vVertices[idx + 2] = 0.0;
+      printf("i:%f idx:%d x:%f,y:%f,z:%f\n", i, idx, vVertices[idx],
+             vVertices[idx + 1], vVertices[idx + 2]);
+      idx += 3;
+    }
 
-   glGenBuffers(1, &vbuffer_);
-	   GL_ERR_CHECK;
-   glBindBuffer(GL_ARRAY_BUFFER, vbuffer_);
-	   GL_ERR_CHECK;
-   glBufferData(GL_ARRAY_BUFFER, 9 *sizeof(GLfloat), &vVertices[0], GL_STATIC_DRAW);
-	   GL_ERR_CHECK;
-   glBindBuffer(GL_ARRAY_BUFFER, 0);
-	   GL_ERR_CHECK;
-   return true;
-}
+    glGenBuffers(1, &vbuffer_);
+    GL_ERR_CHECK;
+    glBindBuffer(GL_ARRAY_BUFFER, vbuffer_);
+    GL_ERR_CHECK;
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW);
+    GL_ERR_CHECK;
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    GL_ERR_CHECK;
+    return true;
+  }
 
-///
-// Draw a triangle using the shader pair created in Init()
-//
-void Draw()
-{
-   // Set the viewport
-   // Use the program object
-   glUseProgram(program_object_);
-	   GL_ERR_CHECK;
+  ///
+  // Draw a triangle using the shader pair created in Init()
+  //
+  void Draw() {
+    // Set the viewport
+    // Use the program object
+    glUseProgram(program_object_);
+    GL_ERR_CHECK;
 
-   // Bind the vertex buffer
-   glBindBuffer(GL_ARRAY_BUFFER, vbuffer_);
-	GL_ERR_CHECK;
+    // Bind the vertex buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vbuffer_);
+    GL_ERR_CHECK;
 
-   // Load the vertex data
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	   GL_ERR_CHECK;
-   glEnableVertexAttribArray(0);
-	   GL_ERR_CHECK;
-   glDrawArrays(GL_TRIANGLES, 0, 3);
-	   GL_ERR_CHECK;
+    // Load the vertex data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    GL_ERR_CHECK;
+    glEnableVertexAttribArray(0);
+    GL_ERR_CHECK;
+    glDrawArrays(GL_LINE_LOOP, 0, N_VERT);
+    GL_ERR_CHECK;
 
-   glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+  }
 
 public:
-	game_impl3() {
-		assert(Init());
-	}
+  game_impl3() { assert(Init()); }
 
-	void draw() {
-		GL_ERR_CHECK;
-		Draw();
-	}
-
+  void draw() {
+    GL_ERR_CHECK;
+    Draw();
+  }
 };
 
+game::game() : me(new game_impl3()) {}
 
-game::game()
-	: me(new game_impl3())
-{}
+game::~game() {}
 
-game::~game()
-{}
-
-void game::draw()
-{
-	me->draw();
-}
+void game::draw() { me->draw(); }
